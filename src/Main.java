397cae903e1f11c2a6b408f2eefe9e5f698b6372
Akteon_ <akteon.akteonov@gmail.com>
2022-10-22 11:41:28 +0300

    homework 6 confirmed completed

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main (String[] args) {
-        //Nothing really important here... I love The Beatles btw.
-    }
-    public static String getSpace(int a) {
-        if (a >= 100) {
-            return String.valueOf(a);
-        } else if (a >= 10) {
-            return " " + a;
-        } else {
-            return "  " + a;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        byte[] mass = new byte[n / 8 + Boolean.compare(n % 8 != 0, false)];
+        for (int i = 0; i < n; ++i) {
+            int bol = (in.nextBoolean() ? 1 : 0);
+            byte rick = (byte) (bol << (i % 8));
+            mass[i / 8] = (byte) (mass[i / 8] | rick);
+        }
+        for (int i = 0; i < n; ++i) {
+            System.out.print(((mass[i / 8] & (1 << (i % 8))) != 0) + " ");
         }
     }
 }

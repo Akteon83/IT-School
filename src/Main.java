8640d10c8a0182488188507e2a4bca1dadd7a98a
Akteon_ <akteon.akteonov@gmail.com>
2022-12-03 19:22:02 +0300

    homework 15 has been successfully committed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        // Извини за костыли)
+        Trinity<Integer> trinity = new Trinity<>(12, 71, 33);
+        System.out.println(trinity.getFirst());
+        System.out.println(trinity.getSecond());
+        System.out.println(trinity.getThird());
+        Trinity<String> neo = new Trinity<>("gonna", "give you up", "never");
+        System.out.println(neo.getFirst());
+        System.out.println(neo.getSecond());
+        System.out.println(neo.getThird());
     }
 }
